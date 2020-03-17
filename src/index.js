102e7d915ca2a7c44a6d15056f1048a876ee2fc0
Student <borodichalexander97@gmail.com>
2020-03-17 17:19:57 +0300

    feat: implement typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) return 0;
+
+  return array.reduce((min, elem) => (min < elem) ? min : elem)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) return 0;
+
+  return array.reduce((max, elem) => (max > elem) ? max : elem)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) return 0;
+
+  return array.reduce((sum, elem) => sum += elem) / array.length;
 }
